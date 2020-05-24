52a66a227773fb39b87deeb386a476f78c24bb47
JAVIER <javidiaz94@gmail.com>
2020-05-24 17:12:27 +0200

    [ADD]: new ui

--- Delhi/src/Application.py
+++ Delhi/src/Application.py
@@ -8,87 +8,100 @@ import window
 import tkinter as tk
 from tkinter import ttk
 
-class HomeFrame(ttk.Frame):     #MANAGER PACK
+class FileFrame(ttk.Frame):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         
-        self.portada = tk.PhotoImage (file = "portada.png")        
-        self.image_label = ttk.Label(self, image = self.portada).pack(side = 'top')
+        self.new_icon = tk.PhotoImage (file = "newicon.png")        
+        self.new_label = ttk.Label(self, image = self.new_icon, text = "NEW").grid(row = 0, column = 0)
 
-class SolveFrame(ttk.Frame):    #MANAGER GRID
+class EditFrame(ttk.Frame):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         
-        self.label1 = ttk.Label(self, text = "Label 1")
-        self.label1.grid(row = 0, column = 0, padx=30, pady=30)
-        
-        self.label2 = ttk.Label(self, text = "Label 2")
-        self.label2.grid(row = 1, column = 0)
-        
-        self.help_button = ttk.Button(self, text="Show Help", command =self.show_help)
-        self.help_button.grid(row = 2, column = 0)
-        
-        self.equation1_entry = ttk.Entry(self, width = 25)
-        self.equation1_entry.grid(row = 0, column = 1)
-        self.equation1_entry.insert(0, "Type your fist equation here")
-        
-        self.equation2_entry = ttk.Entry(self, width = 25)
-        self.equation2_entry.grid(row = 1, column = 1)
-        self.equation2_entry.insert(0, "Type your second equation here")
-        
-        self.help_label = ttk.Label(self)
-        self.help_label.grid(row = 2, column = 1)
+#         self.label1 = ttk.Label(self, text = "Label 1")
+#         self.label1.grid(row = 0, column = 0, padx=30, pady=30)
+#         
+#         self.label2 = ttk.Label(self, text = "Label 2")
+#         self.label2.grid(row = 1, column = 0)
+#         
+#         self.help_button = ttk.Button(self, text="Show Help", command =self.show_help)
+#         self.help_button.grid(row = 2, column = 0)
+#         
+#         self.equation1_entry = ttk.Entry(self, width = 25)
+#         self.equation1_entry.grid(row = 0, column = 1)
+#         self.equation1_entry.insert(0, "Type your fist equation here")
+#         
+#         self.equation2_entry = ttk.Entry(self, width = 25)
+#         self.equation2_entry.grid(row = 1, column = 1)
+#         self.equation2_entry.insert(0, "Type your second equation here")
+#         
+#         self.help_label = ttk.Label(self)
+#         self.help_label.grid(row = 2, column = 1)
         
         # self.solution = ttk.Label(self, text = "Label 2")
         # self.solution.pack()
         
-    def show_help(self):
-        if (self.help_label["text"] == ""):
-            self.help_label["text"] = ("Press this button to hide this.")
-        else: self.help_label["text"] = ""
+#     def show_help(self):
+#         if (self.help_label["text"] == ""):
+#             self.help_label["text"] = ("Press this button to hide this.")
+#         else: self.help_label["text"] = ""
 
-class ModulesFrame(ttk.Frame):
+class HelpFrame(ttk.Frame):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
             
+class SolverFrame(ttk.Frame):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
 
-class Application(ttk.Frame):   #MANAGER GRID
+class Application(ttk.Frame):
     
     def __init__(self, root):
         super().__init__(root)
-        root.title("DELHI")        
+        root.title("DELHI")
         
-        root.geometry("{}x{}+{}+{}".format(
-            window.WINDOW_WIDTH, 
-            window.WINDOW_HEIGHT, 
-            window.X_COORDINATE, 
-            window.Y_COORDINATE))
-        root.resizable(True, True)
-        
-        #Ahora el notebook para tener pestañas     
         self.notebook = ttk.Notebook(
-            self, width = window.WINDOW_WIDTH -25, height = window.WINDOW_HEIGHT -45)
+            self, width = 400, height = 100) #window.WINDOW_HEIGHT -45 de alto, window.WINDOW_WIDTH -25 de ancho
         
-        self.home_frame = HomeFrame(self.notebook)
+        self.new_icon = tk.PhotoImage (file = "newicon.png")
+        self.new_tab_img = tk.PhotoImage (file = "newlabel.png")
+        
+        self.file_frame = FileFrame(self.notebook)
         self.notebook.add(
-                self.home_frame, text="Home", padding=10)
+                self.file_frame, text = "File", padding=10) #, image = self.new_tab_img
         
-        self.solver_frame = SolveFrame(self.notebook)
+        self.edit_frame = EditFrame(self.notebook)
         self.notebook.add(
-                self.solver_frame, text="Solver", padding=10)
+                self.edit_frame, text="Edit", padding=10)
         
-        self.mod1_frame = ModulesFrame(self.notebook)
+        self.help_frame = HelpFrame(self.notebook)
         self.notebook.add(
-            self.mod1_frame, text="Module 1", padding=10)
+            self.help_frame, text="Help", padding=10)
         
-        self.mod2_frame = ModulesFrame(self.notebook)
-        self.notebook.add(
-            self.mod2_frame, text="Module 2", padding=10)
+        s = ttk.Style()
+        s.configure('LateralMenu.TFrame', background = 'lightorange')
+        s.configure('SolverFrame.TFrame', background = 'dodgerblue')
+        s.configure('tools.TFrame', background = "lightblue")
         
-        self.mod3_frame = ModulesFrame(self.notebook)
-        self.notebook.add(
-            self.mod3_frame, text="Module 3", padding=10)
+        self.lateral_menu = ttk.Frame(self, width = 100, height = 426, style = 'LateralMenu.TFrame')
+        self.solver_frame = ttk.Frame(self, width = 300, height = 300, style = 'SolverFrame.TFrame')
         
-        self.notebook.grid(row = 0, column = 0, padx=10, pady=10)
+        self.lateral_menu.grid(row = 0, column = 0, rowspan = 2, sticky = "NSWE")
+        self.notebook.grid(row = 0, column = 1, sticky = "NSWE")
+        self.solver_frame.grid(row = 1, column = 1, sticky = "NSWE")
         self.grid(row=0, column = 0)
+        
+    def toggleFullScreen(self, event):
+        root.fullScreenState = not root.fullScreenState
+        root.window.attributes("-fullscreeen", root.fullScreenState)
+        
+    def quitFullScreen(self, event):
+        root.fullScreenState = False
+        root.window.attributes("-fullscreeen", root.fullScreenState)
+        
+        
+        
+        
+        
         
--- Delhi/src/main.py
+++ Delhi/src/main.py
@@ -4,14 +4,100 @@ Created on 5 mar. 2020
 @author: Javier
 '''
 
-import tkinter as tk
+from tkinter import *
+from tkinter import ttk
 import Application
 
-if __name__ == '__main__':
+def show_solver():
+    practice.grid_remove()
+    stepbystep.grid_remove()
+    solver.grid(column=0, row=0, sticky = "nsew")
     
-    print("This is a change to test GIT commits")
-    print("Second comment")
-    print("Third comment")
-    root = tk.Tk()
-    app = Application.Application(root)
-    app.mainloop()
+def show_practice():
+    solver.grid_remove()
+    stepbystep.grid_remove()
+    practice.grid(column=0, row=0, sticky = "nsew")
+    
+def show_stepbystep():
+    solver.grid_remove()
+    practice.grid_remove()
+    stepbystep.grid(column=0, row=0, sticky = "nsew")
+        
+if __name__ == '__main__':
+        
+    root = Tk()
+    root.title("DELHI Solver")
+#     app = Application.Application(root)
+#     app.mainloop()
+    s = ttk.Style()
+    s.configure('lateral.TFrame', background='lightblue')
+    s.configure('solver.TFrame', background="dodgerblue")
+    s.configure('practice.TFrame', background="orange")
+    s.configure('stepbystep.TFrame', background="lightgreen")
+    
+    content = ttk.Frame(root)
+    lateral = ttk.Frame(content, relief ="sunken", width=100, height=400, style="lateral.TFrame")
+    notebook = ttk.Frame(content, relief ="sunken", width=500, height=100)
+    workarea = ttk.Frame(content, relief ="sunken", width=500, height=300)
+    solver = ttk.Frame(workarea, width=500, height=300, style="solver.TFrame")
+    practice = ttk.Frame(workarea, width=500, height=300, style="practice.TFrame")
+    stepbystep = ttk.Frame(workarea,width=500, height=300, style="stepbystep.TFrame")
+    
+    etsiiupmicon = PhotoImage (file = "images/etsii.png")
+    etsiilbl = ttk.Label(lateral, image = etsiiupmicon)
+    etsiilbl.pack(side = TOP, expand = False)
+    workoptions = ttk.Frame(lateral, relief = "sunken", width=80, height = 200)
+    workoptions.pack(side = TOP, expand = True, fill = BOTH)
+    solverbtn = ttk.Button(workoptions, text = " Solver ", command = show_solver)
+    practicebtn = ttk.Button(workoptions, text = " Practice ", command = show_practice)
+    stepsbtn = ttk.Button(workoptions, text = " Step-by-step ", command = show_stepbystep)
+    solverbtn.pack(side = TOP, expand = True, fill = BOTH)
+    practicebtn.pack(side = TOP, expand = True, fill = BOTH)
+    stepsbtn.pack(side = TOP, expand = True, fill = BOTH)
+    
+    cuaderno = ttk.Notebook(notebook, width = 300, height = 50)
+    page1 = ttk.Frame(cuaderno)
+    page2 = ttk.Frame(cuaderno)
+    page3 = ttk.Frame(cuaderno)
+    cuaderno.add(page1, text=" File ", padding=5)
+    cuaderno.add(page2, text=" Edit ", padding=5)
+    cuaderno.add(page3, text=" Help ", padding=5)
+    
+    newbtn = ttk.Button(page1, text = "New")
+    openbtn = ttk.Button(page1, text = "Open")
+    savebtn =  ttk.Button(page1, text = "Save")
+    saveasbtn = ttk.Button(page1, text = "Save As")
+    closebtn = ttk.Button(page1, text = "Close")
+    newbtn.pack(side = LEFT, expand = True, fill = BOTH)
+    openbtn.pack(side = LEFT, expand = True, fill = BOTH)
+    savebtn.pack(side = LEFT, expand = True, fill = BOTH)
+    saveasbtn.pack(side = LEFT, expand = True, fill = BOTH)
+    closebtn.pack(side = LEFT, expand = True, fill = BOTH)
+    
+    cutbtn = ttk.Button(page2, text = "Cut").pack(side = LEFT, expand = True, fill = BOTH)
+    copybtn = ttk.Button(page2, text = "Copy").pack(side = LEFT, expand = True, fill = BOTH)
+    pastebtn = ttk.Button(page2, text = "Paste").pack(side = LEFT, expand = True, fill = BOTH)
+    undobtn = ttk.Button(page2, text = "Undo").pack(side = LEFT, expand = True, fill = BOTH)
+    redobtn = ttk.Button(page2, text = "Redo").pack(side = LEFT, expand = True, fill = BOTH)  
+    
+    tutobtn = ttk.Button(page3, text = "Tutorial").pack(side = LEFT, expand = True, fill = BOTH)  
+    aboutbtn = ttk.Button(page3, text = "About").pack(side = LEFT, expand = True, fill = BOTH)
+    
+    content.grid(column=0, row=0, sticky="nsew")
+    lateral.grid(column=0, row=0, rowspan=3, sticky="nsew")
+    notebook.grid(column=1, row=0, columnspan=2, sticky="nsew")
+    cuaderno.pack(expand = "true", fill="both")
+    workarea.grid(column=1, row=1, columnspan=2, rowspan=2, sticky="nsew")
+    
+    root.columnconfigure(0, weight=1)
+    root.rowconfigure(0, weight=1)
+    content.grid_columnconfigure(0, weight=1)
+    content.grid_columnconfigure(1, weight=20)
+    content.grid_rowconfigure(0, weight=1)
+    content.grid_rowconfigure(1, weight=5)
+    notebook.grid_columnconfigure(0, weight=1)
+    notebook.grid_columnconfigure(1, weight=1)
+    workarea.grid_columnconfigure(0, weight=1)
+    workarea.grid_rowconfigure(0, weight=1)
+    
+    root.mainloop()
